d2b81fa874bc2a22cacae79e4bd800e1aa17b388
cabamarcos <marcos.caballero.0203@gmail.com>
2025-08-24 20:14:42 +0200

    Proyectos terminado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
+                  
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
